a9f836ecb02a374e24f36f8eea2d5a2a4c5a0b13
sidney <ojsidney044@gmail.com>
2024-11-13 11:10:42 +0300

    made the submit button reactgit add .

--- js/script.js
+++ js/script.js
@@ -1,23 +1,46 @@
+
+
+const form = document.querySelector('#enter-message')
 const emailInfo = document.querySelector('#email-info')
-// const submit1 = document.querySelector('#submit1')
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
 
-// submit1.addEventListener("click", () => {
-//     alert('message sent, check your email for response')
-// })
-emailInfo.addEventListener('submit', (event)=>{
-    event.preventDefault()
-
-    validateForm()
+    validateForm();
 })
 
 function validateForm(){
-    if(emailInfo.Value.trim()=='')
-        setError(emailInfo, 'email cannot be empty' )
+    if(emailInfo.value.trim() == '')
+    setError(emailInfo, 'Provide email address' );
 }
-function setError(element, errorMessage){
-const parent = element.parentElement
-parent.classList.add('error')
+// else if(isEmailValid(emailInfo.value)){
+//     setSuccess(emailInput)
+// }else{
+//     setError(emailInput, 'provide valid email address')
+// }
+
+ function setError(element, errorMessage){
+const parent = element.parentElement;
+if (parent.classList.contains('success')){
+    parent.classList.remove('success')
+}
+parent.classList.add('error');
 const paragraph = parent.querySelector('p')
 paragraph.textContent = errorMessage
+ }
+
+function setSuccess(element){
+    const parent = element.parentElement;
+    if(parent.classList.contains('error')){
+        parent.classList.remove('error')
+    }
+    parent.classList.add('success')
 }
+
+// function isEmailValid(email){
+//     const reg = ^[^@]+@[^@]+\.[^@]+$
+
+
+//     return reg.test(email)
+// }
+
