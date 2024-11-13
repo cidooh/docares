c16bff0923714b12d99e4485278efed82010b970
sidney <ojsidney044@gmail.com>
2024-11-13 15:29:03 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 
 
 const form = document.querySelector('#enter-message')
+const textArea = document.querySelector('#text-area')
 const emailInfo = document.querySelector('#email-info')
 const hamburger = document.querySelector('.hamburger')
-const navMenu = document.querySelector('nav-menu')
+const navMenu = document.querySelector('.nav-menu')
 
 
 hamburger.addEventListener("click", () => {
@@ -16,6 +17,8 @@ document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", (
     navMenu.classList.remove("active")
 }))
 
+
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -24,7 +27,7 @@ form.addEventListener('submit', (event) => {
 
 function validateForm(){
     if(emailInfo.value.trim() == '')
-    setError(emailInfo, 'Provide email address' )
+    setError(emailInfo, 'Provide email address')
 }
  if(isEmailValid(emailInfo.value)){
     setSuccess(emailInput)
@@ -32,6 +35,10 @@ function validateForm(){
 else{
     setError(emailInput, 'provide valid email address')
 }
+if(textArea.value.trim() == ''){
+    setError(textArea, 'add your message')
+}
+
 
  function setError(element, errorMessage){
 const parent = element.parentElement;
@@ -51,10 +58,10 @@ function setSuccess(element){
     parent.classList.add('success')
 }
 
-// function isEmailValid(email){
-//     const reg = ^[^@]+@[^@]+\.[^@]+$
+function isEmailValid(email){
+    const reg =(/^[A-Za-z\._\-0-9]*[@][A_Za-z]*[\.][a-z]{2,4}$/)
 
 
-//     return reg.test(email)
-// }
+    return reg.test(email)
+}
 
