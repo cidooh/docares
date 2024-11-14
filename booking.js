9616bffea1dfd3709c81fd2044710e476bf6af42
sidney <ojsidney044@gmail.com>
2024-11-14 10:40:09 +0300

    made all the pages responsive

--- booking.js
+++ booking.js
@@ -20,6 +20,23 @@
 //     }
 
 // Open modal and populate doctor name
+
+
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () =>{
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+}))
+
+
 function openBookingModal(doctorName) {
     document.getElementById('doctor').value = doctorName;
     document.getElementById('bookingModal').style.display = "block";
