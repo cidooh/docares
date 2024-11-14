6185988c8b825ac1bdd82719e1253c7249c102bf
sidney <ojsidney044@gmail.com>
2024-11-14 12:49:33 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,67 +1,60 @@
-
-
-const form = document.querySelector('#enter-message')
-const textArea = document.querySelector('#text-area')
-const emailInfo = document.querySelector('#email-info')
-const hamburger = document.querySelector('.hamburger')
-const navMenu = document.querySelector('.nav-menu')
-
+const form = document.querySelector("#enter-message");
+const textArea = document.querySelector("#text-area");
+const emailInfo = document.querySelector("#email-info");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
 
-document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () =>{
-    hamburger.classList.remove("active")
-    navMenu.classList.remove("active")
-}))
+document.querySelectorAll("nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    validateForm();
-})
+  validateForm();
+});
 function validateForm() {
-       if (emailInfo.value.trim() == '') {
-        setError(emailInfo, 'Provide email address')
-    } 
-   
-    else if (isEmailValid(emailInfo.value)) {
-        setSuccess(emailInfo)
-    } 
-   
-    else {
-        setError(emailInfo, 'Provide a valid email address')
-    }
-      if (textArea.value.trim() == '') {
-        setError(textArea, 'Add your message')
-    }
+  if (emailInfo.value.trim() == "") {
+    setError(emailInfo, "Provide email address");
+  } else if (isEmailValid(emailInfo.value)) {
+    setSuccess(emailInfo);
+  } else {
+    setError(emailInfo, "Provide a valid email address");
+  }
+  if (textArea.value.trim() == "") {
+    setError(textArea, "Add your message");
+  }
+ 
 }
 
- function setError(element, errorMessage){
-const parent = element.parentElement;
-if (parent.classList.contains('success')){
-    parent.classList.remove('success')
-}
-parent.classList.add('error');
-const paragraph = parent.querySelector('p')
-paragraph.textContent = errorMessage
- }
-
-function setSuccess(element){
-    const parent = element.parentElement;
-    if(parent.classList.contains('error')){
-        parent.classList.remove('error')
-    }
-    parent.classList.add('success')
+function setError(element, errorMessage) {
+  const parent = element.parentElement;
+  if (parent.classList.contains("success")) {
+    parent.classList.remove("success");
+  }
+  parent.classList.add("error");
+  const paragraph = parent.querySelector("p");
+  paragraph.textContent = errorMessage;
 }
 
-function isEmailValid(email){
-    const reg =(/^[A-Za-z\._\-0-9]*[@][A_Za-z]*[\.][a-z]{2,4}$/)
-
-    return reg.test(email)
+function setSuccess(element) {
+  const parent = element.parentElement;
+  if (parent.classList.contains("error")) {
+    parent.classList.remove("error");
+  }
+  parent.classList.add("success");
 }
 
+function isEmailValid(email) {
+  const reg = /^[A-Za-z\._\-0-9]*[@][A_Za-z]*[\.][a-z]{2,4}$/;
+
+  return reg.test(email);
+}
